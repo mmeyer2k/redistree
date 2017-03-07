7ad11f54ca9b03c5cb39154cf5f29e0b95ff7103
Michael M <m.meyer2k@gmail.com>
2017-03-07 14:00:38 -0600

    Update statistics.blade.php

--- views/statistics.blade.php
+++ views/statistics.blade.php
@@ -1,22 +1,22 @@
 @extends('redistree::layout')
 
-@section('title') RedisTree - Statistics @endsection
+@section('title', 'RedisTree - Statistics')
 
 @section('content')
-<div class="panel panel-default">
-    <div class="panel-heading">
-        <h2>
-            <i class="fa fa-line-chart"></i>
-            Statistics
-        </h2>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h2>
+                <i class="fa fa-line-chart"></i>
+                Statistics
+            </h2>
+        </div>
+        @foreach($info as $section => $data)
+            <div class="panel-heading">
+                <h4>
+                    {{ $section }}
+                </h4>
+            </div>
+            {!! \Mmeyer2k\RedisTree\RedisTreeModel::array2table($data) !!}
+        @endforeach
     </div>
-    @foreach($info as $section => $data)
-    <div class="panel-heading">
-        <h4>
-            {{ $section }}
-        </h4>
-    </div>
-    {!! \Mmeyer2k\RedisTree\RedisTreeModel::array2table($data) !!}
-    @endforeach
-</div>
 @endsection
